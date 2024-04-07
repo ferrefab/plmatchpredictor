3b6f1ec9b53657e88ab82698036e9deda312ea9c
ferrefab <126014200+ferrefab@users.noreply.github.com>
2024-04-07 21:42:59 +0200

    Delete scraper directory

--- scraper/mongo_import.py
+++
@@ -1,5 +0,0 @@
-import pymongo as MongoClient
-import pandas as pd
-
-client = MongoClient('mongodb+srv://mongodb:Acesso8105@mdmmongodbmain.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000')
-
