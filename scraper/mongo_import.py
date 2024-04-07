536c8a5d7e4faaf6be2440ac8756f38db9c6dcf0
ferrefab <126014200+ferrefab@users.noreply.github.com>
2024-04-07 21:46:33 +0200

    Add files via upload

---
+++ scraper/mongo_import.py
@@ -0,0 +1,5 @@
+import pymongo as MongoClient
+import pandas as pd
+
+client = MongoClient('mongodb+srv://mongodb:Acesso8105@mdmmongodbmain.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000')
+
